e5b3fcc91b0a3bfa1eff67da65d9614678fe2764
Anton-U01 <toni.urdjanov@abv.bg>
2024-06-09 17:53:22 +0300

    Needed classes for the login

---
+++ src/main/java/softuni/bg/pathfinder/services/CurrentUser.java
@@ -0,0 +1,32 @@
+package softuni.bg.pathfinder.services;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Component
+@SessionScope
+public class CurrentUser {
+    private String fullName;
+    private boolean isLoggedIn;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public boolean isLoggedIn() {
+        return isLoggedIn;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        isLoggedIn = loggedIn;
+    }
+
+    public void reset(){
+        this.fullName = null;
+        this.isLoggedIn = false;
+    }
+}
